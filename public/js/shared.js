69550dd90aaadcee107cdf6a9fbe18e29f40d305
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-06-27 19:33:00 +0330

    search in global

--- public/js/shared.js
+++ public/js/shared.js
@@ -2,4 +2,17 @@ import { getAndShowSocials } from "../../utils/shared.js";
 
 window.addEventListener("load", () => {
   getAndShowSocials();
+  
+  
+  
+    const global_search_input = document.getElementById("global_search_input")
+    console.log(global_search_input);
+    global_search_input?.addEventListener("keyup" , (e) => {
+        if(e.keyCode === 13)  {
+          e.preventDefault() ;
+          location.href = "posts.html"
+          location.href = `posts.html?value=${e.target.value}`   
+        }
+    })
+
 });
